82032a50488202b541cc76ea69e0ee2d102fbd68
temathugga <lowskilldoter@mail.ru>
2026-04-30 17:06:47 +0300

    commit

---
+++ WpfApp1/WpfApp1/LoginWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string login = txtLogin.Text;
+            string password = txtPassword.Password;
+
+            if (string.IsNullOrEmpty(login) && string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Пожалуйста, введите логин и пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(login))
+            {
+                MessageBox.Show("Пожалуйста, введите логин", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Пожалуйста, введите пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (login == "admin" && password == "admin")
+            {
+                MessageBox.Show("Успешная авторизация", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClients_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new Pages.ClientsPage());
+            //txtTitle.Text = "Клиенты";
+        }
+
+        private void btnCars_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new Pages.CarsPage());
+            //txtTitle.Text = "Автомобили";
+        }
+
+        private void btnTariffs_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new Pages.TariffsPage());
+            //txtTitle.Text = "Тарифы";
+        }
+
+        private void btnServices_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new Pages.ServicesPage());
+            //txtTitle.Text = "Услуги";
+        }
+        private void btnOrders_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new Pages.OrdersPage());
+            //txtTitle.Text = "Заказы";
+        }
+
+        private void btnReports_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new Pages.WorkReportsPage());
+            //txtTitle.Text = "Отчеты о работах";
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
